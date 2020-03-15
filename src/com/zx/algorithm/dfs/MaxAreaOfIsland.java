3a9a93107406bf902bc78f20b25b1e11f730a88f
Jcduhdt <15150651313@126.com>
2020-03-15 21:44:46 +0800

    leedcode 深度遍历，沉岛思想 主要还是递归这东西，没往这方面想

---
+++ src/com/zx/algorithm/dfs/MaxAreaOfIsland.java
@@ -0,0 +1,54 @@
+package com.zx.algorithm.dfs;
+
+/**
+ * @author ZhangXiong
+ * @version v12.0.1
+ * @date 2020-03-15
+ * 力扣 695
+ * 深度优先搜索 沉岛思想
+ * 给定一个包含了一些 0 和 1的非空二维数组 grid , 一个岛屿是由四个方向 (水平或垂直) 的1(代表土地) 构成的组合。
+ * 你可以假设二维矩阵的四个边缘都被水包围着。
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0。)
+ * 给大佬跪了
+ */
+public class MaxAreaOfIsland {
+    public static void main(String[] args) {
+        int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        MaxAreaOfIsland area = new MaxAreaOfIsland();
+        System.out.println(area.maxAreaOfIsland(grid));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(i, j, grid));
+                }
+            }
+        }
+        return res;
+    }
+    // 每次调用的时候默认num为1，进入后判断如果不是岛屿，则直接返回0，就可以避免预防错误的情况。
+    // 每次找到岛屿，则直接把找到的岛屿改成0，这是传说中的沉岛思想，就是遇到岛屿就把他和周围的全部沉默。
+    // ps：如果能用沉岛思想，那么自然可以用朋友圈思想。有兴趣的朋友可以去尝试。
+    private int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int num = 1;
+        num += dfs(i + 1, j, grid); // 向上搜索
+        num += dfs(i - 1, j, grid); // 向下搜索
+        num += dfs(i, j + 1, grid); // 向右搜索
+        num += dfs(i, j - 1, grid); // 向左搜索
+        return num;
+    }
+}
